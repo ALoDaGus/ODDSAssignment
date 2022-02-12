c82fb15dd29fa76b87815fa3d8159d62cb007b4f
TPmac <tp@192.168.0.102>
2022-02-12 16:33:59 +0700

    V2

--- main.py
+++ main.py
@@ -1,44 +1,81 @@
 import re
 
+EVEN_PREFIX = 'Even'
+ODD_PREFIX = 'Odd'
+
 def a(number):
     snumber = str(number)
     result = ''
     for i in snumber:
         if int(i) % 2 == 0:
-            result += 'Even' + i
+            result += EVEN_PREFIX + i
         else:
-            result += 'Odd' + i
+            result += ODD_PREFIX + i
 
     return result
 
 
-def b(param):
-    param = param.replace('Odd', 'DDO')
-    param = param.replace('Even', 'NEVE')
-    return param
+def b(my_input):
+    result = ''
+    stack = []
+    for i in str(my_input):
+        if i.isdigit():
+            for j in stack[:]:
+                result += stack.pop()
+            result += i
+        else:
+            stack.append(i.upper())
+
+    return result
 
 
-def c(param):
-    param = param.replace('DDO', '686879')
-    param = param.replace('NEVE', '78698669')
-    return param
+def c(my_input):
+    result = ''
+    for i in my_input:
+        if i.isdigit():
+            result += str(i)
+            continue
+        result += str(ord(i))
+
+    return result
 
 
-def d(param):
-    param = param.replace('686879', 'DDO')
-    param = param.replace('78698669', 'NEVE')
-    return param
+def d(my_input):
+    result = ''
+    index = 0
+    while True:
+        if index > len(my_input)-1:
+            break
+        if my_input[index] == '6':
+            result += 'DDO' + my_input[index + 6]
+            index += 7
+        else:
+            result += 'NEVE' + my_input[index + 8]
+            index += 9
+
+    return result
 
         
-def e(param):
-    param = param.replace('DDO', 'Odd')
-    param = param.replace('NEVE', 'Even')
-    return param
-
-
-def f(param):
+def e(my_input):
     result = ''
-    numberlist = re.findall(r'\d+', param)
+    stack = []
+    for index, i in enumerate(my_input):
+        if i.isdigit():
+            for j in stack[:]:
+                result += stack.pop()
+            result += i
+        else:
+            if my_input[index + 1].isdigit():
+                stack.append(i.upper())
+                continue
+            stack.append(i.lower())
+
+    return result
+
+
+def f(my_input):
+    result = ''
+    numberlist = re.findall(r'\d+', my_input)
     for i in numberlist:
         result += i
 
@@ -46,10 +83,24 @@ def f(param):
 
 
 if __name__ == '__main__':
-    number = input('Your number :')
-    print('function a => ' + a(number))
-    print('function b => ' + b(a(number)))
-    print('function c => ' + c(b(a(number))))
-    print('function d => ' + d(c(b(a(number)))))
-    print('function e => ' + e(d(c(b(a(number))))))
-    print('function f => ' + f(e(d(c(b(a(number)))))))
+    
+    while True:
+      number = input('Your number :')
+      if not number.isdigit():
+          print('please try again')
+          continue
+      break
+    
+    a = a(number)
+    b = b(a)
+    c = c(b)
+    d = d(c)
+    e = e(d)
+    f = f(e)
+    
+    print('function a => ' + a)
+    print('function b => ' + b)
+    print('function c => ' + c)
+    print('function d => ' + d)
+    print('function e => ' + e)
+    print('function f => ' + f)
